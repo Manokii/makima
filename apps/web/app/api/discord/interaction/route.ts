8ad12f4fd4bf0295df5af7b4dd8cfa7b9a388d93
Jasper Concepcion <jasper@manok.dev>
2024-10-10 16:09:11 +0800

    chore: route test

--- apps/web/app/api/discord/interaction/route.ts
+++ apps/web/app/api/discord/interaction/route.ts
@@ -2,7 +2,26 @@ import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic"; // defaults to auto
 
+const InteractionType = {
+  PING: 1,
+  APPLICATION_COMMAND: 2,
+  MESSAGE_COMPONENT: 3,
+  APPLICATION_COMMAND_AUTOCOMPLETE: 4,
+  MODAL_SUBMIT: 5,
+} as const;
+type IntearctionType = (typeof InteractionType)[keyof typeof InteractionType];
+
+const InteractionContextType = {
+  GUILD: 0,
+  BOT_DM: 1,
+  PRIVATE_CHANNEL: 2,
+} as const;
+type InteractionContextType =
+  (typeof InteractionContextType)[keyof typeof InteractionContextType];
+
 export async function POST(request: Request) {
-  console.log(request);
+  const body = await request.json();
+  console.log("Request XXXX", request);
+  console.log("Body XXXX", body);
   return NextResponse.json({ message: "Hello, World!", request });
 }
