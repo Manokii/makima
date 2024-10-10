68a21439428bd06302e9306763d2ea38019e63ae
Jasper Concepcion <jasper@manok.dev>
2024-10-10 15:44:15 +0800

    chore: add discord interaction webhook

--- apps/web/app/api/discord/interaction/route.ts
+++ apps/web/app/api/discord/interaction/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic"; // defaults to auto
 
-export async function GET(request: Request) {
-  return NextResponse.json({ message: "Hello, World!" });
+export async function POST(request: Request) {
+  console.log(request);
+  return NextResponse.json({ message: "Hello, World!", request });
 }
